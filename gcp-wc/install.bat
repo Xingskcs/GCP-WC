c9d034091dc2a5d4413d1dcfb7544535c11a9afc
Xingskcs <csskxing@163.com>
2017-08-03 07:23:06 +0000

    modified:   gcp-wc/install.bat 	new file:   gcp-wc/install2017-08-03_110951.log 	new file:   gcp-wc/install2017-08-03_144719.log 	modified:   gcp-wc/registerZookeeper/__pycache__/registerZookeeperService.cpython-35.pyc 	modified:   gcp-wc/uninstall.bat 	new file:   gcp-wc/uninstall2017-08-03_ 	new file:   gcp-wc/uninstall2017-08-03_123546.log 	modified:   gcp-wc/updateResources/__pycache__/updateResourcesService.cpython-35.pyc

--- gcp-wc/install.bat
+++ gcp-wc/install.bat
@@ -1,11 +1,11 @@
 @echo off
 set today=%date:~0,4%-%date:~5,2%-%date:~8,2%
-@call :output>install%today%_%time:~0,2%%time:~3,2%%time:~6,2%.log
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
+@call :output>install%today%_%Hour%%Minute%%Second%.log
 exit
 :output
 
 for /f %%i in ('git tag') do set tag1=%%i
-git pull origin master
 for /f %%j in ('git tag') do set tag2=%%j
 
 IF %tag1%==%tag2% (
--- gcp-wc/uninstall.bat
+++ gcp-wc/uninstall.bat
@@ -1,6 +1,7 @@
 @echo off
 set today=%date:~0,4%-%date:~5,2%-%date:~8,2%
-@call :output>uninstall%today%_%time:~0,2%%time:~3,2%%time:~6,2%.log
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
+@call :output>uninstall%today%_%Hour%%Minute%%Second%.log
 exit
 :output
 
