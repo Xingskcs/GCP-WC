baf74de31c1f5b4e5df3047cf75e257b1a965ae5
Xingskcs <csskxing@163.com>
2017-08-30 11:43:41 +0800

    modified:   gcp-wc/install_check.bat

--- gcp-wc/install_check.bat
+++ gcp-wc/install_check.bat
@@ -1,11 +1,7 @@
 for /f %%i in ('git tag') do set tag1=%%i
+echo %tag1%
+git pull origin master
 for /f %%j in ('git tag') do set tag2=%%j
+echo %tag2%
 
-IF %tag1%==%tag2% (
-    call install_run.bat
-) ELSE (
-    git reset --hard %tag1%
-    call uninstall.bat
-    git pull origin master
-    call install_run.bat
 )
