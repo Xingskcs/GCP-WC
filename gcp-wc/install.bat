91cab0030ea2e3eca91f4c8bc147feebd1d0c873
Xingskcs <csskxing@163.com>
2017-08-30 11:35:46 +0800

    modified:   gcp-wc/install.bat

--- gcp-wc/install.bat
+++ gcp-wc/install.bat
@@ -2,5 +2,5 @@
 for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
 
 for /f "delims=" %%i in ('call install_check.bat') do (
-echo [%Year%/%Month%/%Day%, %Hour%:%Minute%:%Second%] %%i >> install%Year%-%Month%-%Day%_%Hour%%Minute%%Second%.log
+echo [%Year%-%Month%-%Day%, %Hour%:%Minute%:%Second%] %%i >> install%Year%-%Month%-%Day%_%Hour%%Minute%%Second%.log
 )
